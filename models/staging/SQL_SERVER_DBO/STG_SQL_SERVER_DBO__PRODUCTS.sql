facdbb16006b69798e52aab2c4603fa3832bbe21
Alejandro Martínez Jiménez <amj.parrales@gmail.com>
2025-11-08 17:41:12 +0000

    modelado de PRODUCTS, corrección de nombres de STG

--- models/staging/SQL_SERVER_DBO/STG_SQL_SERVER_DBO__PRODUCTS.sql
+++ models/staging/SQL_SERVER_DBO/STG_SQL_SERVER_DBO__PRODUCTS.sql
@@ -5,20 +5,21 @@
   )
 }}
 
-WITH src_budget AS (
+WITH products_normalizado AS (
     SELECT * 
     FROM {{ source('SQL_SERVER_DBO', 'PRODUCTS') }}
     ),
 
-renamed_casted AS (
+products_casted AS (
     SELECT
-     	PRODUCT_ID ,
-	PRICE ,
-	NAME ,
-	INVENTORY ,
-	_FIVETRAN_DELETED ,
-	_FIVETRAN_SYNCED ,
-    FROM src_budget
+        PRODUCT_ID ,
+        cast(PRICE AS decimal (10,2)) AS PRICE ,
+        md5(lower(trim(NAME))) AS NAME_HASH,
+        trim(NAME) AS NAME,
+        INVENTORY ,
+        _FIVETRAN_DELETED ,
+        CONVERT_TIMEZONE('Europe/Madrid', _FIVETRAN_SYNCED::TIMESTAMP_NTZ) AS FIVETRAN_SYNCED
+    FROM products_normalizado
     )
 
-SELECT * FROM renamed_casted
+SELECT * FROM products_casted
